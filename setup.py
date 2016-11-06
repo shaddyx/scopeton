bea895d2099fb8d5dd2df114d7785ceefa99e7a5
ayakushko <ayakushko@intropro.com>
2016-11-06 12:02:38 +0200

    [-] git repo url changed

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
   description = 'Dependency injection mechanism',
   author = 'Anatolii Yakushko',
   author_email = 'shaddyx@gmail.com',
-  url = 'https://github.com/shaddyx/dipy', # use the URL to the github repo
+  url = 'https://github.com/shaddyx/scopeton', # use the URL to the github repo
   download_url = 'https://github.com/shaddyx/scopeton/tarball/0.1',
   keywords = ['dependency', 'injection'], # arbitrary keywords
   classifiers = []
