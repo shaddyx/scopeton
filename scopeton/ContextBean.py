315136b019dc9677aa2905a8ee89507f64a62f4f
Anatolii Yakushko <ayakushko@intropro.com>
2016-11-21 14:47:19 +0200

    [-] improved ContextBean repr method

--- scopeton/ContextBean.py
+++ scopeton/ContextBean.py
@@ -4,3 +4,7 @@ class ContextBean(object):
     postConstruct = None
     preDestroy = None
     lazy = False
+    def __str__(self):
+        return "ContextBean[{self.name}](lazy={self.lazy})".format(self=self)
+    def __repr__(self):
+        return self.__str__()
