bd7debb8c31d9e606ad2454a7fdd1a88a02ebb60
Anatolii Yakushko <ayakushko@intropro.com>
2016-11-28 11:26:27 +0200

    version fixed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'scopeton',
   packages = ['scopeton'],
-  version = '0.15',
+  version = '0.150',
   description = 'Dependency injection mechanism',
   author = 'Anatolii Yakushko',
   author_email = 'shaddyx@gmail.com',
