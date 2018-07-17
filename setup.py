5088ac2c598acd4cd1c89a63a49575a5595a709e
Anatolii Yakushko <shaddyx@gmail.com>
2018-07-17 21:40:20 +0300

    [-] changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'scopeton',
   packages = ['scopeton'],
-  version = '1.001',
+  version = '1.002',
   description = 'Dependency injection mechanism',
   author = 'Anatolii Yakushko',
   author_email = 'shaddyx@gmail.com',
